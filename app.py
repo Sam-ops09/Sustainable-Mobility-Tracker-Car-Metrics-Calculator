0e1dd2fe1c345e7fffe5297719fd3ef9baa87b24
Samanyu <72296412+Sam-ops09@users.noreply.github.com>
2023-09-22 12:02:53 +0530

    compare.html path and page made

--- app.py
+++ app.py
@@ -99,5 +99,70 @@ def graph_representation():
     # You can now pass fuel_consumption_data and makes_list to the template
     return render_template('graph.html', fuel_consumption_data=fuel_consumption_data, makes=makes_list, selected_make=selected_make)
 
+# Define a function to read the available models from the CSV file
+def get_available_models():
+    available_models = set()  # Use a set to ensure unique model names
+
+    with open('FuelConsumption.csv', 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            available_models.add(row['MODEL'])
+
+    return sorted(list(available_models))  # Sort the models alphabetically
+
+# Modify the get_unique_makes function to also get models
+def get_unique_makes():
+    makes_and_models = {}
+    with open('FuelConsumption.csv', 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            make = row['MAKE']
+            model = row['MODEL']
+            if make not in makes_and_models:
+                makes_and_models[make] = [model]
+            else:
+                makes_and_models[make].append(model)
+    return makes_and_models
+
+# Function to get model specifications based on make and model
+def get_model_specs(make, model):
+    specs = {}  # Initialize an empty dictionary to store model specifications
+
+    # Open and read the CSV file
+    with open('FuelConsumption.csv', 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            if row['MAKE'] == make and row['MODEL'] == model:
+                # Assign the specifications to the dictionary
+                specs['Make'] = row['MAKE']
+                specs['Model'] = row['MODEL']
+                specs['Fuel Consumption Comb (L/100 km)'] = row['FUELCONSUMPTION_COMB']
+                specs['CO2 Emissions (g/km)'] = row['CO2EMISSIONS']
+                specs['Engine Size (L)'] = row['ENGINESIZE']
+                specs['Cylinders'] = row['CYLINDERS']
+                specs['Vehicle Class'] = row['VEHICLECLASS']
+                specs['Transmission'] = row['TRANSMISSION']
+
+    return specs
+
+# Modify the /compare route
+@app.route('/compare', methods=['GET', 'POST'])
+def compare():
+    makes_and_models = get_unique_makes()
+
+    if request.method == 'POST':
+        make1 = request.form.get('make1')
+        model1 = request.form.get('model1')
+        make2 = request.form.get('make2')
+        model2 = request.form.get('model2')
+
+        specs1 = get_model_specs(make1, model1)
+        specs2 = get_model_specs(make2, model2)
+
+        return render_template('compare.html', makes_and_models=makes_and_models, specs1=specs1, specs2=specs2)
+
+    return render_template('compare.html', makes_and_models=makes_and_models)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
